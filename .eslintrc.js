ac99e4588c507c777fbb15784a3ef01b1c46b50e
temilaj <temilaj@hotmail.com>
2022-01-17 18:47:28 -0500

    ch(App setup): setup linter and code formatter

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', './src'],
+      },
+    },
+  },
+  rules: {
+    // 'import/no-unresolved': 'off',
+    'import/no-import-module-export': 'off',
+  },
+};
