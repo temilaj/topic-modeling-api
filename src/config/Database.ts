7546d35beb8a12cc7190f9bb78cb37478929a02b
temilaj <temilaj@hotmail.com>
2022-01-19 01:56:55 -0500

    ch(setup): initialize database config

---
+++ src/config/Database.ts
@@ -0,0 +1,43 @@
+import mongoose, { Model, Connection, MongooseOptions } from 'mongoose';
+
+import { IUserDocument, User } from '../models/User';
+
+import config from './environments';
+import logger from './logger';
+
+export interface IDatabase {
+  User: Model<IUserDocument>;
+}
+
+export const models: IDatabase = {
+  User,
+};
+
+export const connectToDB = (databaseURI: string): Connection => {
+  const connect = async () => {
+    const databaseOptions = {
+      reconnectTries: Number.MAX_VALUE,
+      bufferTimeoutMS: 500,
+      minPoolSize: 10,
+    };
+
+    mongoose.Promise = global.Promise;
+    if (config.isDevelopment) {
+      mongoose.set('debug', true);
+    }
+    await mongoose.connect(databaseURI, databaseOptions).catch((error) => {
+      logger.error(`error creating connection to ${databaseURI}`, error);
+      return process.exit(1);
+    });
+  };
+  connect();
+
+  mongoose.connection.on('disconnected', () => {
+    logger.info(`disconnected from ${databaseURI}`);
+    connect();
+  });
+  mongoose.connection.on('connected', () => logger.info(`Monoose connected to ${databaseURI} successfully`));
+  mongoose.connection.on('error', logger.error);
+
+  return mongoose.connection;
+};
---
+++ src/models/User.ts
@@ -0,0 +1,63 @@
+import { Document, Schema, model } from 'mongoose';
+
+export interface IUserDocument extends Document {
+  id: string;
+  _id: string | object;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  resetToken: string;
+  activationToken: string;
+  resetTokenExpiry: number;
+  refreshTokenVersion: number;
+  lastLoginDate: number;
+  imageUrl: string;
+  createdAt: Date;
+  updatedAt: Date | number;
+}
+
+const UserSchema: Schema<IUserDocument> = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    resetToken: String,
+    resetTokenExpiry: Number,
+    activationToken: String,
+    refreshTokenVersion: {
+      type: Number,
+      default: 0,
+    },
+    lastLoginDate: Number,
+    imageUrl: { type: String, default: '', trim: true },
+  },
+  {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  },
+);
+
+UserSchema.index({ createdAt: 1 });
+
+export const User = model<IUserDocument>('User', UserSchema);
